4add5d18cbec9d46ed55351e45b946f946f4a68b
LotuxPunk <clement@vandendaelen.com>
2017-11-11 01:58:11 +0100

    add permission samtime

--- src/com/vandendaelen/simpleautomessage/CommandSamTime.java
+++ src/com/vandendaelen/simpleautomessage/CommandSamTime.java
@@ -3,22 +3,27 @@ package com.vandendaelen.simpleautomessage;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 public class CommandSamTime implements CommandExecutor {
-	
+
 	Plugin plugin;
-	
+
 	public CommandSamTime(Plugin pl) {
 		plugin = pl;
 	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		plugin.getConfig().set("Time", Integer.parseInt(args[0]));
-		plugin.saveConfig();
-		plugin.reloadConfig();
-		return true;
+		Player p = (Player)sender;
+		if(p.hasPermission("simpleautomessage.settime")||p.isOp()) {
+			plugin.getConfig().set("Time", Integer.parseInt(args[0]));
+			plugin.saveConfig();
+			plugin.reloadConfig();
+			return true;
+		}
+		return false;
 	}
 
 }
